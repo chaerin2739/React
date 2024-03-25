b668287db9f031bc04fbcc3e683792918a1fe66e
YOO CHAERIN <chlin2739@naver.com>
2024-03-25 19:15:30 +0900

    React.js 입문

---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,17 @@
+const Button = ({text, color, children}) => {
+
+    return <button 
+    onClick={() => {
+        console.log(text);
+    }}
+    style={{color:color}}>
+        {text} - {color.toUpperCase()}
+        {children}
+        </button>
+};
+
+Button.defaultProps = {
+    color: "black",
+};
+
+export default Button;
--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -5,15 +5,24 @@
 // 3. 모든 태그는 닫혀있어야 한다.
 // 4. 최상위 태그는 반드시 하나여야만 한다.
 
-const Main = () => {
-    const number = 11;
 
+import "./Main.css"
+
+
+const Main = () => {
+
+    const user = {
+        name : "유채린",
+        isLogin : true,
+    }
     return (
-        <main> 
-            <h1>main</h1>
-            <h2>{number % 2 ===0 ? "짝수" : "홀수"}</h2>
-        </main>
+       <>
+       {user.isLogin ? (
+       <div className="login">로그인</div>
+       ) : (
+       <div>로그아웃</div>)}
+       </>
     );
-};
+ };
 
 export default Main;
